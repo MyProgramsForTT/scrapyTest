33565164a28c3e05de3b0ff0a59d2d93eb34141d
Sourcery AI <>
2022-10-25 13:19:58 +0000

    'Refactored by Sourcery'

--- scrapyTest/spiders/index.py
+++ scrapyTest/spiders/index.py
@@ -10,7 +10,7 @@ class IndexSpider(scrapy.Spider):
     def parse(self,response):
         type_url = response.xpath("//ul[@id='menuul']/li/a/@href")
         for i in type_url:
-            start_url = 'https://www.yikm.net/'+ str(type_url)
+            start_url = f'https://www.yikm.net/{str(type_url)}'
             yield scrapy.Request(start_url,callback=self.type_list)
 
     def type_list(self, response):
@@ -18,22 +18,22 @@ class IndexSpider(scrapy.Spider):
         #print(response.request.headers['User-Agent'])
         next_url_data = response.xpath("/html/body/article/nav[2]/ul/li[2]/a/@href").extract_first()
         data_all = response.xpath("//div[@class='row']/div[@class='col-md-3 col-xs-6']")
+        none = " "
         for i in data_all:
             play_url = i.xpath(".//a/@href").extract_first()
             play_imgurl = i.xpath(".//a//img[@class='img img-raised']/@src").extract_first()
             play_title = i.xpath(".//div[@class='table']/span/text()").extract()
-            none = " "
             for title_all in play_title:
                 all = title_all+none
             all+=all
             play_name = i.xpath(".//div[@class='table']/h4/a/text()").extract_first()
-            item['游戏地址'] = 'https://www.yikm.net' + play_url,
+            item['游戏地址'] = (f'https://www.yikm.net{play_url}', )
             item['游戏名字'] =  play_name,
             item['游戏类型'] =  all,
             item['游戏图片'] = play_imgurl
             yield item
             if next_url_data is not "#":
-                next_url = 'https://www.yikm.net' + next_url_data
+                next_url = f'https://www.yikm.net{next_url_data}'
                 next_url = urllib.parse.urljoin(response.url,next_url)
                 yield scrapy.Request(next_url,callback=self.type_list,meta={'item': item})
 
